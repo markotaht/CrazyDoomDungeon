05fcd4288e6c7ce658fb53675627e0b5f094448b
marko <markotaht@gmail.com>
2017-03-03 21:18:10 +0200

    Player follows where mouse is clicked.

--- Crazy Doom Dungeon/Assets/Scripts/PlayerController.cs
+++ Crazy Doom Dungeon/Assets/Scripts/PlayerController.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Vector3 target;
     // Use this for initialization
 	void Start () {
+        target = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * 6;
-        var z = Input.GetAxisRaw("Vertical") * Time.deltaTime * 6;
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Physics.Raycast(ray, out hit);
 
-        Vector3 direction = Quaternion.AngleAxis(0,Vector3.up) * new Vector3(x, 0, z);
-        
-        transform.Translate(direction);
+            target = hit.point;
+        }
+        if(Vector3.Distance(transform.position,target) > 1.1) { 
+            Vector3 direction = target- transform.position;
+            direction.y = 0;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        Physics.Raycast(ray, out hit);
-        transform.LookAt(new Vector3(hit.point.x,transform.position.y,hit.point.z));
-
-	}
+            transform.rotation = Quaternion.LookRotation(direction.normalized);
+            transform.Translate(Vector3.forward * Time.deltaTime * 6);
+        }
+        else
+        {
+            target = transform.position;
+        }
+       
+    }
 }
